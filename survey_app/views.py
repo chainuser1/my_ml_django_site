9af16f8805fa02c49cf8279f1082786ab3ae8970
Jayson Ramos <lothlorien654@gmail.com>
2021-07-08 10:48:39 +0800

    Modify model DateTimeField to IntegerField

--- survey_app/views.py
+++ survey_app/views.py
@@ -7,9 +7,19 @@ from django.http import JsonResponse
 from .models import Product, Category, Log
 from django.contrib.auth.models import User
 from datetime import datetime
+from django.core.serializers import serialize
+import json, time, numpy as np
+
+
+
+#transaction function writer
+t_date = int(time.mktime(datetime.utcnow().timetuple())*1000)
+
+def write_transaction_log(request,log_name, log_details):
+    Log.objects.create(transaction_name=log_name,
+                       transaction_details=log_details,
+                       user=request.user)
 # Create your views here.
-
-
 @login_required(redirect_field_name='next', login_url = 'login:login_do')
 @never_cache
 def home(request):
@@ -36,6 +46,7 @@ def create(request):
         try:
             category = Category.objects.get(pk=category_id)
             new_product = category.product_set.create(name=name,price=price,stock=stock)
+            write_transaction_log(request,'Product Added', new_product.name  + " has been added")
             return JsonResponse({'message':new_product.name + ' has been created.'})
         except Category.DoesNotExist:
             return JsonResponse({'message':'An error occurred.'})
@@ -51,11 +62,9 @@ def update(request,id):
             price = request.POST["price"],
             category= request.POST["category"],
             stock = request.POST["stock"],
-            updated_at = datetime.now())
+            updated_at = t_date)
             pro_obj = Product.objects.filter(pk=id).get()
-            Log.objects.create(transaction_name="Product Update",
-                       transaction_details=pro_obj.name + ' has been updated.',
-                       user=request.user)
+            write_transaction_log(request,'Product Update', pro_obj.name  + " has been updated")
             
             return JsonResponse({'message': pro_obj.name + ' has been updated.'})
         except Product.DoesNotExist:
@@ -68,14 +77,25 @@ def delete(request, id):
         try:
             obj_deleted = Product.objects.filter(pk=id).get()
             Product.objects.filter(pk=id).delete()
+            write_transaction_log(request,'Product Deleted', obj_deleted.name  + " has been deleted")
             return JsonResponse({'message': obj_deleted.name + ' was successfully deleted.'})
         except Product.DoesNotExist:
             return JsonResponse({'message': 'An error occurred while deleting product. This product is not found'})
 
 
+
+
 @login_required(redirect_field_name='next', login_url = 'login:login_do')
 def show_logs(request):
-    logs = Log.objects.all().order_by("-created_at").values()
-    logs=list(logs)
-    return JsonResponse({'logs':logs}, safe=False)
+    logs = Log.objects.all()
+    logs = serialize('json',logs,use_natural_foreign_keys=True)
+    return JsonResponse({'logs':json.loads(logs)}, safe=False)
 
+
+@login_required(redirect_field_name='next', login_url = 'login:login_do')
+def show_numpy_data_products(requests):
+    products =  Product.objects.all()
+    vlqs = products.values_list()
+    my_list = list(vlqs)
+    r = np.array(my_list)
+    return r
