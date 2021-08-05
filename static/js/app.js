3c57f0d38b93da581a4053c5823894eba1ea5e63
Jayson Ramos <lothlorien654@gmail.com>
2021-08-05 12:28:09 +0800

    Add Cart Notif

--- static/js/app.js
+++ static/js/app.js
@@ -1,25 +1,5 @@
 
-toastr.options = {
-   "closeButton": false,
-   "debug": false,
-   "newestOnTop": true,
-   "progressBar": false,
-   "positionClass": "toast-top-center",
-   "preventDuplicates": true,
-   "showDuration": "300",
-   "hideDuration": "1000",
-   "timeOut": "5000",
-   "extendedTimeOut": "1000",
-   "showEasing": "swing",
-   "hideEasing": "linear",
-   "showMethod": "fadeIn",
-   "hideMethod": "fadeOut"
- }
 
-
-
-
-const hostname = window.location.origin + window.location.pathname
 
 
 var c = new Vue({
